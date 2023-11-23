5a8b0c6cc874009653b580123aa457fb6268f646
shirguin <shirguin@yandex.ru>
2023-11-23 06:45:52 +0300

    try_catch

--- OOP/OOP.cpp
+++ OOP/OOP.cpp
@@ -1,12 +1,44 @@
 ﻿#include <iostream>
 #include <string>
+#include <fstream>
 #include "Point.h"
+
 
 using namespace std;
 
+void Foo(int value)
+{
+	if (value < 0)
+	{
+		//throw exception("Число меньше 0!");
+		throw "Число меньше 0!";
+
+	}
+
+	if (value = 0)
+	{
+
+	}
+
+	cout << "Переменная = " << value << endl;
+}
+
 int main()
 {
-    Point point(10, 20, 30);
-    point.Print();
+	setlocale(LC_ALL, "ru");
+
+	try
+	{
+		Foo(-55);
+	}
+	catch (const exception &ex)
+	{
+		cout << "Мы поймали " << ex.what()<< endl;
+	}
+	catch (const char* ex)
+	{
+		cout << "Блок 2 мы поймали" << ex << endl;
+	}
+
 }
 
